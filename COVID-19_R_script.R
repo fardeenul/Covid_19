73d276acd6c4889fffc0c85110e9b798b1a6157d
fardeenul <106887632+fardeenul@users.noreply.github.com>
2023-07-10 00:10:28 -0300

    Add files via upload

---
+++ COVID-19_R_script.R
@@ -0,0 +1,41 @@
+library(Hmisc) # import
+install.packages("Hmisc")
+
+data <- COVID19_line_list_data
+
+# Display the first few rows of the data
+head(data)
+describe(data) # Hmisc command
+
+# cleaned up death column
+data$death_dummy <- as.integer(data$death != 0)
+# death rate
+(sum(data$death_dummy) / nrow(data)) * 100
+
+
+# AGE
+# claim: people who die are older
+dead = subset(data, death_dummy == 1)
+alive = subset(data, death_dummy == 0)
+mean(dead$age, na.rm = TRUE)
+mean(alive$age, na.rm = TRUE)
+# is this statistically significant?
+t.test(alive$age, dead$age, alternative="two.sided", conf.level = 0.95)
+# normally, if p-value < 0.05, we reject null hypothesis
+# here, p-value ~ 0, so we reject the null hypothesis and 
+# conclude that this is statistically significant
+
+# GENDER
+# claim: people who die are older
+men <- subset(dead, gender == "male")
+women <- subset(alive, gender == "female")
+mean(men$death_dummy, na.rm = TRUE) #8.5%!
+mean(women$death_dummy, na.rm = TRUE) #3.7%
+
+# is this statistically significant?
+t.test(men$death_dummy, women$death_dummy, alternative="two.sided", conf.level = 0.99)
+# 99% confidence: men have from 0.8% to 8.8% higher chance
+# of dying.
+# p-value = 0.002 < 0.05, so this is statistically
+# significant
+
